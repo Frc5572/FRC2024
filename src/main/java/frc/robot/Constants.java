cf73db442914e1721118a8966d2b7cbe5ef2ca81
syroxius <likhiyenamala@gmail.com>
2024-01-27 09:50:43 -0600

    Added PID's

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,7 +1,6 @@
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-import com.ctre.phoenix6.hardware.TalonFX;
+
 
 /**
  * Constants file.
--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -1,10 +1,14 @@
 package frc.robot.subsystems.shooter;
 
 import org.littletonrobotics.junction.Logger;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class Shooter extends SubsystemBase {
     private ShooterIO io;
+    private PIDController pid =
+        new PIDController(Constants.Shooter.kP, Constants.Shooter.kI, Constants.Shooter.kD);
     private ShooterIOInputsAutoLogged inputs = new ShooterIOInputsAutoLogged();
 
     public Shooter(ShooterIO io) {
@@ -14,6 +18,9 @@ public class Shooter extends SubsystemBase {
     public void periodic() {
         io.updateInputs(inputs);
         Logger.processInputs("Shooter", inputs);
+        double VoltageOutput = pid.calculate(inputs.shooterVelocityRotPerSecond);
+        io.setTopMotor(VoltageOutput);
+        io.setBottomMotor(VoltageOutput);
     }
 
     public void setTopMotor(double power) {
