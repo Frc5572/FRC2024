f6b7f6d2c78958abedd4afa9296a6ed7afa68946
Omar Martinez Hernandez <115192927+Rango813@users.noreply.github.com>
2024-01-27 15:36:43 -0600

    Add Swerve Subsystem (#14)

---
+++ src/main/java/frc/lib/util/swerve/SwerveModuleIO.java
@@ -0,0 +1,29 @@
+package frc.lib.util.swerve;
+
+import org.littletonrobotics.junction.AutoLog;
+import com.ctre.phoenix6.controls.ControlRequest;
+
+/** IO Class for SwerveModule */
+public interface SwerveModuleIO {
+    /** Inputs Class for SwerveModule */
+    @AutoLog
+    public static class SwerveModuleInputs {
+        public double driveMotorSelectedPosition;
+        public double driveMotorSelectedSensorVelocity;
+        public double angleMotorSelectedPosition;
+        public double absolutePositionAngleEncoder;
+    }
+
+    public default void updateInputs(SwerveModuleInputs inputs) {}
+
+    public default void setDriveMotor(ControlRequest request) {}
+
+    public default void setAngleMotor(ControlRequest request) {}
+
+    public default void setAngleSelectedSensorPosition(double angle) {}
+
+    public default void setPositionAngleMotor(double absolutePosition) {}
+
+
+
+}
