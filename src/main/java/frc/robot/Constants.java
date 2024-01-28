fb40bd9b4b955694d91986337282412a268d410b
Reed Mullett <120128467+Merridew1@users.noreply.github.com>
2024-01-27 18:28:23 -0600

    Elevator wrist commands (#17)
    Co-authored-by: Alex Resnick <adr8292@gmail.com>

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -218,29 +218,58 @@ public final class Constants {
      * Class for elevator and wrist constants
      */
     public static final class ElevatorWristConstants {
-        public static final int ELEVATOR_ENC_CHANNEL_A = -1;
-        public static final int ELEVATOR_ENC_CHANNEL_B = -1;
-        public static final int TOP_LIMIT_SWITCH_PORT = -1;
-        public static final int BOTTOM_LIMIT_SWITCH_PORT = -1;
 
-        public static final double ELEVATOR_KP = 0;
-        public static final double ELEVATOR_KI = 0;
-        public static final double ELEVATOR_KD = 0;
-        public static final double ELEVATOR_MAX_VELOCITY = 0;
-        public static final double ELEVATOR_MAX_ACCELERATION = 0;
-        public static final double ELEVATOR_KS = 0;
-        public static final double ELEVATOR_KG = 0;
-        public static final double ELEVATOR_KV = 0;
+        /**
+         * Sensor Constants
+         */
+        public static final class Sensors {
+
+            public static final int ELEVATOR_ENC_CHANNEL_A = -1;
+            public static final int ELEVATOR_ENC_CHANNEL_B = -1;
+            public static final int TOP_LIMIT_SWITCH_PORT = -1;
+            public static final int BOTTOM_LIMIT_SWITCH_PORT = -1;
+        }
+
+        /**
+         * PID constants
+         */
+        public static final class PID {
+
+            public static final double ELEVATOR_KP = 0;
+            public static final double ELEVATOR_KI = 0;
+            public static final double ELEVATOR_KD = 0;
+            public static final double ELEVATOR_MAX_VELOCITY = 0;
+            public static final double ELEVATOR_MAX_ACCELERATION = 0;
+            public static final double ELEVATOR_KS = 0;
+            public static final double ELEVATOR_KG = 0;
+            public static final double ELEVATOR_KV = 0;
+
+            public static final double WRIST_KP = 0;
+            public static final double WRIST_KI = 0;
+            public static final double WRIST_KD = 0;
+            public static final double WRIST_MAX_VELOCITY = 0;
+            public static final double WRIST_MAX_ACCELERATION = 0;
+            public static final double WRIST_KS = 0;
+            public static final double WRIST_KG = 0;
+            public static final double WRIST_KV = 0;
+        }
+
+        /**
+         * Set points constants for elevator and wrist
+         */
+        public static final class SetPoints {
+
+            public static final double HOME_HEIGHT = 0;
+            public static final double HOME_ANGLE = 0;
+            public static final double AMP_HEIGHT = 0;
+            public static final double AMP_ANGLE = 0;
+            public static final double TRAP_HEIGHT = 0;
+            public static final double TRAP_ANGLE = 0;
+            public static final double CLIMBING_HEIGHT = 0;
+            public static final double CLIMBING_ANGLE = 0;
+        }
 
 
-        public static final double WRIST_KP = 0;
-        public static final double WRIST_KI = 0;
-        public static final double WRIST_KD = 0;
-        public static final double WRIST_MAX_VELOCITY = 0;
-        public static final double WRIST_MAX_ACCELERATION = 0;
-        public static final double WRIST_KS = 0;
-        public static final double WRIST_KG = 0;
-        public static final double WRIST_KV = 0;
     }
     /**
      * Pneumatics CAN id constants.
