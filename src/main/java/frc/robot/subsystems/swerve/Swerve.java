3bb42a4be9f7696c8c7271f7a5f7d65cd4485110
Gael <gvaldez7206@stu.neisd.net>
2024-01-31 18:56:51 -0600

    finished

--- src/main/java/frc/robot/subsystems/swerve/Swerve.java
+++ src/main/java/frc/robot/subsystems/swerve/Swerve.java
@@ -260,11 +260,10 @@ public class Swerve extends SubsystemBase {
      * @return the difference between the pose of speaker and swerve
      */
     public double distanceFromSpeaker(Supplier<Pose2d> pose2d) {
-        double distance = Math.sqrt(Math.pow(FieldConstants.Speaker.centerSpeakerOpening.getX(), 2)
-            + Math.pow(FieldConstants.Speaker.centerSpeakerOpening.getY(), 2));
-        double pose =
-            Math.sqrt(Math.pow(pose2d.get().getX(), 2) + Math.pow(pose2d.get().getY(), 2));
+        double distance = Math.sqrt(Math.pow(
+            Math.abs(FieldConstants.Speaker.centerSpeakerOpening.getY() - pose2d.get().getY()), 2)
+            + Math.pow(FieldConstants.Speaker.centerSpeakerOpening.getX(), 2));
 
-        return Math.abs(distance - pose);
+        return distance;
     }
 }
