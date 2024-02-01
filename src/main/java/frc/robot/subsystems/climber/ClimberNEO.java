3cf66346182e599cb54e09202b883a8083fdf2e0
Owen Flatman <74746499+owenflatman@users.noreply.github.com>
2024-01-31 18:23:12 -0600

    Climber Subsytem (#26)
    * start on the climber
    
    * Progress from the other branch + climb height
    
    * lint
    
    * linting
    
    * spell check
    
    * javadoc
    
    * power comment
    
    * Added encoder and outputs
    
    * lint
    
    * linting
    
    * created a seperate pid contrller for each motor
    
    * Made auto climb command, might be right.
    
    * lint
    
    * Added shooter constant
    
    * spell
    
    * resolved conflicts
    
    * lint
    
    * lint
    
    * linitng
    
    * resolved comments
    
    ---------
    
    Co-authored-by: Aidan Underwood <aidanunderwood1@icloud.com>

---
+++ src/main/java/frc/robot/subsystems/climber/ClimberNEO.java
@@ -0,0 +1,49 @@
+package frc.robot.subsystems.climber;
+
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import frc.robot.Constants;
+
+/**
+ * Subsystem for physical climber.
+ */
+public class ClimberNEO implements ClimberIO {
+    public final CANSparkMax leftMotor =
+        new CANSparkMax(Constants.Motors.Climber.LEFT_MOTOR_ID, MotorType.kBrushless);
+    public final CANSparkMax rightMotor =
+        new CANSparkMax(Constants.Motors.Climber.RIGHT_MOTOR_ID, MotorType.kBrushless);
+    public final RelativeEncoder leftRelativeEncoder = leftMotor.getEncoder();
+    public final RelativeEncoder rightRelativeEncoder = leftMotor.getEncoder();
+
+
+    /**
+     * Constructor for climberNEO subsystem.
+     */
+    public ClimberNEO() {
+        rightMotor.follow(leftMotor, true);
+        leftRelativeEncoder.setPositionConversionFactor(25);
+        rightRelativeEncoder.setPositionConversionFactor(25);
+    }
+
+    @Override
+    public void updateInputs(ClimberInputs inputs) {
+        inputs.climberLeftMotorVoltage = leftMotor.getBusVoltage();
+        inputs.climberLeftMotorAmp = leftMotor.getOutputCurrent();
+        inputs.climberRightMotorVoltage = rightMotor.getBusVoltage();
+        inputs.climberRightMotorAmp = rightMotor.getOutputCurrent();
+        inputs.leftMotorEncoderValue = leftRelativeEncoder.getPosition();
+        inputs.rightMotorEncoderValue = rightRelativeEncoder.getPosition();
+
+    }
+
+    @Override
+    public void setLeftClimberVoltage(double voltage) {
+        leftMotor.setVoltage(voltage);
+    }
+
+    @Override
+    public void setRightClimberVoltage(double voltage) {
+        leftMotor.setVoltage(voltage);
+    }
+}
