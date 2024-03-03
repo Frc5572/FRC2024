d82def6ddb4d20bca8f94d4116d81b46001bc1c9
Alex Resnick <adr8292@gmail.com>
2024-03-03 12:05:30 -0600

    Flip Speaker Pose2d for Red allliance

--- src/main/java/frc/robot/subsystems/elevator_wrist/ElevatorWrist.java
+++ src/main/java/frc/robot/subsystems/elevator_wrist/ElevatorWrist.java
@@ -199,10 +199,12 @@ public class ElevatorWrist extends SubsystemBase {
      * @return Rotation of the wrist
      */
     public Rotation2d getAngleFromDistance(Pose2d position) {
-        double distFromSpeaker = position.getTranslation()
-            .minus(FieldConstants.Speaker.centerSpeakerOpening.getTranslation()).getNorm();
-        SmartDashboard.putNumber("Dist from speaker", distFromSpeaker);
-        return Rotation2d.fromDegrees(radiusToAngle.get(distFromSpeaker));
+        Pose2d speakerPos =
+            FieldConstants.allianceFlip(FieldConstants.Speaker.centerSpeakerOpening);
+        double distFromSpeaker =
+            position.getTranslation().minus(speakerPos.getTranslation()).getNorm();
+        SmartDashboard.putNumber("Dist from speaker", Math.abs(distFromSpeaker));
+        return Rotation2d.fromDegrees(radiusToAngle.get(Math.abs(distFromSpeaker)));
     }
 
     /**
