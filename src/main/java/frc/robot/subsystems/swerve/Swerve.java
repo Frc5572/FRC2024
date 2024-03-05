e27fe9f667173e570b8c80292479a38597a6f102
Alex Resnick <adr8292@gmail.com>
2024-03-06 01:37:16 +0200

    tuning (#88)
    Co-authored-by: owenflatman <owenjflatman@gmail.com>

--- src/main/java/frc/robot/subsystems/swerve/Swerve.java
+++ src/main/java/frc/robot/subsystems/swerve/Swerve.java
@@ -215,7 +215,7 @@ public class Swerve extends SubsystemBase {
      */
     public void resetFieldRelativeOffset() {
         // gyro.zeroYaw();
-        fieldOffset = getGyroYaw().getDegrees();
+        fieldOffset = getGyroYaw().getDegrees() + 180;
     }
 
     /**
