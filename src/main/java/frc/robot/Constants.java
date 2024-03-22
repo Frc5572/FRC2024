652a352d944513d5eb21fc34022398e202bee944
Gael <gvaldez7206@stu.neisd.net>
2024-03-21 20:06:13 -0500

    new elevator motor

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -77,7 +77,8 @@ public final class Constants {
          * Class for elevator and wrist motor constants
          */
         public static final class ElevatorWrist {
-            public static final int ELEVATOR_NEO_ID = 57;
+            public static final int ELEVATOR_LEFT_NEO_ID = 57;
+            public static final int ELEVATOR_RIGHT_NEO_ID = 12;
             public static final int WRIST_NEO_ID = 52;
         }
 
