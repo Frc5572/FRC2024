95923535925eea129e0bea6f1a8e97e3e7ecd40f
jace rodgers <jaransas2008@gmail.com>
2025-01-09 19:31:34 -0600

    updated

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "FRC2024";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 65;
+  public static final String GIT_SHA = "59d2068549f670cd6fc45de6995f2a690acef192";
+  public static final String GIT_DATE = "2025-01-04 11:18:32 EST";
+  public static final String GIT_BRANCH = "update";
+  public static final String BUILD_DATE = "2025-01-09 20:30:13 EST";
+  public static final long BUILD_UNIX_TIME = 1736472613387L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
