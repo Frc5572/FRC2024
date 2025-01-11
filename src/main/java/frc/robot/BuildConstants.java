e2e92cdb959c49a6e11bcc79c34186cd2582519c
Your Name <you@example.com>
2025-01-11 13:34:11 -0600

    All of intake is updated to 2025 correctly :D

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 65;
-  public static final String GIT_SHA = "59d2068549f670cd6fc45de6995f2a690acef192";
-  public static final String GIT_DATE = "2025-01-04 11:18:32 EST";
-  public static final String GIT_BRANCH = "update";
-  public static final String BUILD_DATE = "2025-01-09 20:30:13 EST";
-  public static final long BUILD_UNIX_TIME = 1736472613387L;
+  public static final int GIT_REVISION = 67;
+  public static final String GIT_SHA = "e05457c87db8dbc133d1555f4d57105cca1dc889";
+  public static final String GIT_DATE = "2025-01-09 21:59:21 EST";
+  public static final String GIT_BRANCH = "update-intake";
+  public static final String BUILD_DATE = "2025-01-10 09:25:28 EST";
+  public static final long BUILD_UNIX_TIME = 1736519128755L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
