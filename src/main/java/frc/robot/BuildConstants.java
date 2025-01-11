8d0102f5f91de98f696b52b9e8c10f6ccf38b29e
jace rodgers <jaransas2008@gmail.com>
2025-01-11 01:58:51 -0600

    updated motor configs

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 65;
-  public static final String GIT_SHA = "59d2068549f670cd6fc45de6995f2a690acef192";
-  public static final String GIT_DATE = "2025-01-04 11:18:32 EST";
+  public static final int GIT_REVISION = 66;
+  public static final String GIT_SHA = "95923535925eea129e0bea6f1a8e97e3e7ecd40f";
+  public static final String GIT_DATE = "2025-01-09 20:31:34 EST";
   public static final String GIT_BRANCH = "update";
-  public static final String BUILD_DATE = "2025-01-09 20:30:13 EST";
-  public static final long BUILD_UNIX_TIME = 1736472613387L;
+  public static final String BUILD_DATE = "2025-01-11 02:36:18 EST";
+  public static final long BUILD_UNIX_TIME = 1736580978986L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
